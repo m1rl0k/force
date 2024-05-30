dfcac9001cc1f72120723c0745674bc8a4b6ba14
M1RL0K <mirlok@dr.com>
2024-05-29 23:29:49 -0400

    Update forcebutterworthfilter.ino

--- forcebutterworthfilter.ino
+++ forcebutterworthfilter.ino
@@ -1,122 +1,141 @@
 #include <Wire.h>
 #include <Arduino.h>
 
+class ButterworthFilter {
+public:
+  ButterworthFilter() {
+    v[0] = 0;
+    v[1] = 0;
+  }
+  
+  short step(short x) {
+    v[0] = v[1];
+    long tmp = ((((x * 4089446L) >> 6)    // = ( 3.0468747091e-2 * x)
+                + ((v[0] * 3938714L) >> 1) // + ( 0.9390625058 * v[0])
+               ) + 1048576) >> 21;         // round and downshift fixed point /2097152
 
-class filter
-{
-  public:
-    filter()
-    {
-      v[0] = 0;
-      v[1] = 0;
-    }
-  private:
-    short v[2];
-  public:
-    short step(short x)
-    {
-      v[0] = v[1];
-      long tmp = ((((x * 4089446L) >> 6)	//= ( 3.0468747091e-2 * x)
-                   + ((v[0] * 3938714L) >> 1)	//+( 0.9390625058*v[0])
-                  ) + 1048576) >> 21; // round and downshift fixed point /2097152
+    v[1] = (short)tmp;
+    return (short)(v[0] + v[1]);
+  }
 
-      v[1] = (short)tmp;
-      return (short)((
-                       (v[0] + v[1]))); // 2^
-    }
+private:
+  short v[2];
 };
 
+const int fsrPin = A0;  // Analog pin connected to FSR
+const int ledPinR = 9;  // LED red pin for debugging
+const int ledPinG = 10; // LED green pin for debugging
+const int ledPinB = 11; // LED blue pin for debugging
 
-const int fsrPin = A0;
-int sensorVal;
+ButterworthFilter butterworthFilter;
 
+int smoothedVal = 0;
+const int samples = 10; // Number of samples for smoothing
 
-int fsrReading = 0;
-int smoothedVal     = 0;
-int samples         = 4;
-
-
-int fsrVoltage;
-int value;
-
-int resistance;
-
-unsigned long fsrResistance;
-unsigned long fsrConductance;
-long fsrForce;
-
-uint8_t (force) = fsrForce;
-uint16_t (Newtons) = fsrForce;
-
-
-
-
-String beanName = "Ninja";
-
-
+int hits = 0;
 const int numReadings = 100;
-
-
 int readings[numReadings]; // the readings from the analog input
-int index = 0; // the index of the current reading
-int total = 0; // the running total
+int index = 0;             // the index of the current reading
+int total = 0;             // the running total
 int average = 0;
 
-
-uint16_t pinMap[] = {A0};
-uint16_t pinValues[] = {0, 1};
-
-int inByte;
-int incomingByte = 0;
-
-int val;
-
-
-
-
 void setup() {
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  Serial.begin(9600);
+  pinMode(fsrPin, INPUT);
+  pinMode(ledPinR, OUTPUT);
+  pinMode(ledPinG, OUTPUT);
+  pinMode(ledPinB, OUTPUT);
+
+  // Initialize readings array
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = 0;
-
-    
-    
-  Serial.begin(57600);
-  Serial.setTimeout(25);
-
-  
-    
-
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-    
-
-
-
+  }
 }
 
-
-
-
-
 void loop() {
+  int fsrReading = analogRead(fsrPin);
+  int filteredReading = butterworthFilter.step(fsrReading);
 
+  // Update smoothed value
+  smoothedVal = smoothedVal + ((filteredReading - smoothedVal) / samples);
 
+  // Update running average
+  total -= readings[index];
+  readings[index] = smoothedVal;
+  total += readings[index];
+  index = (index + 1) % numReadings;
+  average = total / numReadings;
 
-  fsrReading = analogRead(fsrPin);
-  Serial.print("Analog:");
+  Serial.print("Analog: ");
+  Serial.print(fsrReading);
+  Serial.print(" Filtered Analog: ");
+  Serial.print(filteredReading);
+  Serial.print(" Smoothed Analog: ");
+  Serial.println(average);
 
-  sensorVal = analogRead(fsrReading);
-  smoothedVal = smoothedVal + ((sensorVal - smoothedVal) / samples);
+  // Calculate voltage
+  float voltage = average * (3.3 / 1023.0);
+  Serial.print("Voltage: ");
+  Serial.println(voltage, DEC);
 
-  Serial.print("Smoothed Analog:");
-  Serial.println(smoothedVal);
+  // Calculate resistance
+  float resistance = ((1023.0 - average) * 10000) / average;
+  Serial.print("Resistance: ");
+  Serial.println(resistance, DEC);
 
-  // slow down amount of readings and prints
+  // Calculate force
+  float fsrConductance = 1000000.0 / resistance;
+  float fsrForce;
+  if (fsrConductance <= 1000) {
+    fsrForce = fsrConductance / 80.0;
+  } else {
+    fsrForce = (fsrConductance - 1000) / 30.0;
+  }
+  Serial.print("Force in Newtons: ");
+  Serial.println(fsrForce);
 
+  // Update LED based on force
+  updateLed(fsrForce);
 
+  // Detect hits based on force threshold
+  if (detectHit(fsrForce)) {
+    hits++;
+    Serial.print("Hits: ");
+    Serial.println(hits);
+  }
 
-  float voltage = sensorVal * (3.29 / 1023.0);
+  delay(100);
+}
 
-  Serial.print("Voltage:");
-  Serial.println(voltage);
-   }
+void updateLed(float force) {
+  if (force > 20) {
+    setLedColor(255, 0, 0); // Red
+  } else if (force > 1) {
+    setLedColor(0, 0, 255); // Blue
+  } else if (force > 0) {
+    setLedColor(0, 255, 0); // Green
+  } else {
+    setLedColor(0, 0, 0); // Off
+  }
+}
+
+void setLedColor(uint8_t r, uint8_t g, uint8_t b) {
+  analogWrite(ledPinR, r);
+  analogWrite(ledPinG, g);
+  analogWrite(ledPinB, b);
+}
+
+bool detectHit(float force) {
+  static float lastForce = 0;
+  static unsigned long lastHitTime = 0;
+  const unsigned long debounceTime = 200; // 200 ms debounce time
+
+  unsigned long currentTime = millis();
+  if (force > 5 && (currentTime - lastHitTime > debounceTime)) {
+    lastHitTime = currentTime;
+    lastForce = force;
+    return true;
+  }
+  lastForce = force;
+  return false;
+}
